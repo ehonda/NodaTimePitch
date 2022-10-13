7293442bf7b70f0c261ac85fc8ddcab353c924d7
Dennis Renz <dennis@r-renz.de>
2022-10-13 23:32:39 +0200

    Add first implementation and tests for GetReportPeriod

---
+++ NodaTimePitch.Tests/ZonedDateTimeExtensionsTests.cs
@@ -0,0 +1,77 @@
+﻿using FluentAssertions;
+using NodaTime;
+using NodaTime.Text;
+
+namespace NodaTimePitch.Tests;
+
+public class ZonedDateTimeExtensionsTests
+{
+    // TODO: Further Test cases
+    //          - now is not at 00:00:00
+    //          - Test GetNthIsoDayOfWeekBefore separately
+    //              - Arrive in an invalid interval when converting to zoned
+    //          - parameterize -> test for all days of a week
+    
+    // TODO: Description, name
+    // TODO: Parameterize
+    [Test]
+    [Description("Week before should be returned from Wednesday")]
+    public void Wednesday()
+    {
+        // Calendar for August 2022
+        // Mon Tue Wed Thu Fri Sat Sun
+        //                           2
+        //   3   4   5   6   7   8   9
+        //  10  11  12  13  14  15  16
+        
+        // TODO: Better variable names
+        // TODO: Better date creation?
+        var localDateTime = LocalDateTimePattern.GeneralIso.Parse("2022-10-12T00:00:00").Value;
+        var now = new ZonedDateTime(localDateTime, DateTimeZone.Utc, Offset.Zero);
+
+        var reportPeriod = now.GetReportPeriod();
+
+        var expectedStart = new ZonedDateTime(
+            LocalDateTimePattern.GeneralIso.Parse("2022-10-02T00:00:00").Value,
+            DateTimeZone.Utc,
+            Offset.Zero);
+        var expectedEnd = new ZonedDateTime(
+            LocalDateTimePattern.GeneralIso.Parse("2022-10-08T23:59:59").Value,
+            DateTimeZone.Utc,
+            Offset.Zero);
+        
+        reportPeriod.Start.Should().Be(expectedStart);
+        reportPeriod.End.Should().Be(expectedEnd);
+    }
+    
+    [Test]
+    [Description("Two weeks before should be returned from Tuesday")]
+    public void Tuesday()
+    {
+        // Calendar for September / October 2022
+        // Mon Tue Wed Thu Fri Sat Sun
+        //                          25
+        //  26  27  28  29  30   1   2
+        //   3   4   5   6   7   8   9
+        //  10  11  12  13  14  15  16
+        
+        // TODO: Better variable names
+        // TODO: Better date creation?
+        var localDateTime = LocalDateTimePattern.GeneralIso.Parse("2022-10-11T00:00:00").Value;
+        var now = new ZonedDateTime(localDateTime, DateTimeZone.Utc, Offset.Zero);
+
+        var reportPeriod = now.GetReportPeriod();
+
+        var expectedStart = new ZonedDateTime(
+            LocalDateTimePattern.GeneralIso.Parse("2022-09-25T00:00:00").Value,
+            DateTimeZone.Utc,
+            Offset.Zero);
+        var expectedEnd = new ZonedDateTime(
+            LocalDateTimePattern.GeneralIso.Parse("2022-10-01T23:59:59").Value,
+            DateTimeZone.Utc,
+            Offset.Zero);
+        
+        reportPeriod.Start.Should().Be(expectedStart);
+        reportPeriod.End.Should().Be(expectedEnd);
+    }
+}
---
+++ NodaTimePitch/ZonedDateTimeExtensions.cs
@@ -0,0 +1,34 @@
+﻿using JetBrains.Annotations;
+using NodaTime;
+
+namespace NodaTimePitch;
+
+[PublicAPI]
+public static class ZonedDateTimeExtensions
+{
+    [Pure]
+    public static (ZonedDateTime Start, ZonedDateTime End) GetReportPeriod(this ZonedDateTime now)
+        => now.DayOfWeek switch
+        {
+            < IsoDayOfWeek.Wednesday => now.GetNthReportPeriodBefore(2),
+            _ => now.GetNthReportPeriodBefore(1)
+        };
+
+    [Pure]
+    private static (ZonedDateTime Start, ZonedDateTime End) GetNthReportPeriodBefore(this ZonedDateTime now, uint n)
+        => (
+            now.GetNthIsoDayOfWeekBefore(IsoDayOfWeek.Sunday, n + 1),
+            now.GetNthIsoDayOfWeekBefore(IsoDayOfWeek.Sunday, n) - Duration.FromSeconds(1));
+
+    // TODO: Version for LocalDateTime, only use that one
+    [Pure]
+    public static ZonedDateTime GetNthIsoDayOfWeekBefore(this ZonedDateTime now, IsoDayOfWeek dayOfWeek, uint n) =>
+        n switch
+        {
+            > 1 => now.GetNthIsoDayOfWeekBefore(dayOfWeek, 1).GetNthIsoDayOfWeekBefore(dayOfWeek, n - 1),
+            // TODO: Are there cases where this fails because the offset is wrong?
+            1 => new(now.LocalDateTime.Previous(dayOfWeek), now.Zone, now.Offset),
+            // TODO: Throw on 0 or return now, which one makes more sense?
+            0 => now
+        };
+}
