52cff2b0c54041159512541f5a34745c8b5405a8
Dennis Renz <dennis@r-renz.de>
2022-10-15 12:52:48 +0200

    More extensive test case

--- NodaTimePitch.Tests/LocalDateTimeExtensionsTests.cs
+++ NodaTimePitch.Tests/LocalDateTimeExtensionsTests.cs
@@ -15,13 +15,26 @@ public class LocalDateTimeExtensionsTests
     //  24  25  26  27  28  29  30
     private static class October2022
     {
-        public static IEnumerable<LocalDate> Mondays => LocalDatesForSameWeekday(3);
-        public static IEnumerable<LocalDate> Tuesdays => LocalDatesForSameWeekday(4);
-        public static IEnumerable<LocalDate> Wednesdays => LocalDatesForSameWeekday(5);
-        public static IEnumerable<LocalDate> Thursdays => LocalDatesForSameWeekday(6);
-        public static IEnumerable<LocalDate> Fridays => LocalDatesForSameWeekday(7);
-        public static IEnumerable<LocalDate> Saturdays => LocalDatesForSameWeekday(8);
-        public static IEnumerable<LocalDate> Sundays => LocalDatesForSameWeekday(9);
+        private static IEnumerable<LocalDate> Mondays => LocalDatesForSameWeekday(3);
+        private static IEnumerable<LocalDate> Tuesdays => LocalDatesForSameWeekday(4);
+        private static IEnumerable<LocalDate> Wednesdays => LocalDatesForSameWeekday(5);
+        private static IEnumerable<LocalDate> Thursdays => LocalDatesForSameWeekday(6);
+        private static IEnumerable<LocalDate> Fridays => LocalDatesForSameWeekday(7);
+        private static IEnumerable<LocalDate> Saturdays => LocalDatesForSameWeekday(8);
+        private static IEnumerable<LocalDate> Sundays => LocalDatesForSameWeekday(9);
+
+        public static IEnumerable<LocalDate> LocalDatesWithDayOfWeek(IsoDayOfWeek dayOfWeek)
+            => dayOfWeek switch
+            {
+                IsoDayOfWeek.Monday => Mondays,
+                IsoDayOfWeek.Tuesday => Tuesdays,
+                IsoDayOfWeek.Wednesday => Wednesdays,
+                IsoDayOfWeek.Thursday => Thursdays,
+                IsoDayOfWeek.Friday => Fridays,
+                IsoDayOfWeek.Saturday => Saturdays,
+                IsoDayOfWeek.Sunday => Sundays,
+                _ => throw new ArgumentOutOfRangeException(nameof(dayOfWeek), dayOfWeek, "Invalid day of week provided")
+            };
 
         private static IEnumerable<LocalDate> LocalDatesForSameWeekday(int startDay)
             => DaysForSameWeekday(startDay).Select(day => day.October(2022));
@@ -29,15 +42,40 @@ public class LocalDateTimeExtensionsTests
         private static IEnumerable<int> DaysForSameWeekday(int startDay)
             => Enumerable.Range(0, 4).Select(offset => startDay + offset * 7);
     }
-
-    [Test]
-    [Description("Previous nth saturday is calculated correctly for the last sunday in october 2022.")]
-    public void PreviousNth_saturday_for_last_sunday([Values(0, 1, 2, 3, 4)] int n)
+    
+    private static class ValueSources
     {
-        var lastSunday = October2022.Sundays.Last();
-        var previousSaturday = lastSunday.PreviousNth(IsoDayOfWeek.Saturday, n);
-        var expectedPreviousSaturday = October2022.Saturdays.Reverse().Prepend(lastSunday).ElementAt(n);
+        public static IEnumerable<IsoDayOfWeek> IsoDaysOfWeek => Enum.GetValues<IsoDayOfWeek>().Except(new[]
+        {
+            IsoDayOfWeek.None
+        });
+    }
 
-        previousSaturday.Should().Be(expectedPreviousSaturday);
+    // TODO: Test case for month transition
+    [Test]
+    [Description("Previous nth is calculated correctly for a subset of combinations of source and target days " +
+                 "in october 2022.")]
+    public void PreviousNth_for_combinations_in_october_2022(
+        [ValueSource(typeof(ValueSources), nameof(ValueSources.IsoDaysOfWeek))] IsoDayOfWeek sourceDay,
+        [ValueSource(typeof(ValueSources), nameof(ValueSources.IsoDaysOfWeek))] IsoDayOfWeek targetDay,
+        [Values(0, 1, 2, 3)] int n)
+    {
+        var sourceDate = October2022.LocalDatesWithDayOfWeek(sourceDay).Last();
+        var previousNth = sourceDate.PreviousNth(targetDay, n);
+        
+        // Expected previous nth is calculated as follows:
+        // - For a source date (e.g. Fri 28) take all dates for the given target day (e.g. Sat) that are before
+        //   the source date. In this example that would be calendar days [8, 15, 22].
+        // - Reverse those candidates, as we are "moving backward in time" from the source date
+        // - Prepend the source date, which is returned in the n = 0 case
+        // - Take the nth element to get the expected previous nth
+        var expectedPreviousNth = October2022
+            .LocalDatesWithDayOfWeek(targetDay)
+            .Reverse()
+            .Where(date => date < sourceDate)
+            .Prepend(sourceDate)
+            .ElementAt(n);
+
+        previousNth.Should().Be(expectedPreviousNth);
     }
 }
