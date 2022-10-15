87b17e6f7d2780119baad4161169c11865481fbb
Dennis Renz <dennis@r-renz.de>
2022-10-15 13:07:36 +0200

    Implement PreviousNth non recursively

--- NodaTimePitch/LocalDateTimeExtensions.cs
+++ NodaTimePitch/LocalDateTimeExtensions.cs
@@ -6,12 +6,13 @@ namespace NodaTimePitch;
 [PublicAPI]
 public static class LocalDateTimeExtensions
 {
-    public static LocalDate PreviousNth(this LocalDate now, IsoDayOfWeek dayOfWeek, int n)
+    // TODO: Benchmark recursive version from 52cff2b against this non recursive version with PlusWeeks
+    // TODO: Can we make this Pure by not throwing exceptions? PlusWeeks is marked as Pure but can throw which seems odd
+    public static LocalDate PreviousNth(this LocalDate sourceDate, IsoDayOfWeek targetDayOfWeek, int n)
         => n switch
         {
-            > 1 => now.PreviousNth(dayOfWeek, 1).PreviousNth(dayOfWeek, n - 1),
-            1 => now.Previous(dayOfWeek),
-            0 => now,
+            >= 1 => sourceDate.Previous(targetDayOfWeek).PlusWeeks(-n + 1),
+            0 => sourceDate,
             _ => throw new InvalidOperationException("Can't compute previous nth day of week for negative n")
         };
 }
