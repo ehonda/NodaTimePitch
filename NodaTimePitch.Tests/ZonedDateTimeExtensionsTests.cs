a4e5f4390db2754bb72e0018f665230dca5d9cae
Dennis Renz <dennis@r-renz.de>
2022-10-16 20:33:28 +0200

    Handle case not originating from midnight correctly

--- NodaTimePitch.Tests/ZonedDateTimeExtensionsTests.cs
+++ NodaTimePitch.Tests/ZonedDateTimeExtensionsTests.cs
@@ -7,10 +7,25 @@ namespace NodaTimePitch.Tests;
 public class ZonedDateTimeExtensionsTests
 {
     // TODO: Further Test cases
-    //          - now is not at 00:00:00
-    //          - Test GetNthIsoDayOfWeekBefore separately
-    //              - Arrive in an invalid interval when converting to zoned
     //          - parameterize -> test for all days of a week
+    // TODO: Helper functions to write test cases more cleanly
+    // TODO: Cleanup
+
+    private static void ExpectReportPeriod(string startPattern, string endPattern,
+        (ZonedDateTime Start, ZonedDateTime End) actualReportPeriod)
+    {
+        var expectedStart = new ZonedDateTime(
+            LocalDateTimePattern.GeneralIso.Parse(startPattern).Value,
+            DateTimeZone.Utc,
+            Offset.Zero);
+        var expectedEnd = new ZonedDateTime(
+            LocalDateTimePattern.GeneralIso.Parse(endPattern).Value,
+            DateTimeZone.Utc,
+            Offset.Zero);
+        
+        actualReportPeriod.Start.Should().Be(expectedStart);
+        actualReportPeriod.End.Should().Be(expectedEnd);        
+    }
     
     // TODO: Description, name
     // TODO: Parameterize
@@ -31,17 +46,7 @@ public class ZonedDateTimeExtensionsTests
 
         var reportPeriod = now.GetReportPeriod();
 
-        var expectedStart = new ZonedDateTime(
-            LocalDateTimePattern.GeneralIso.Parse("2022-10-02T00:00:00").Value,
-            DateTimeZone.Utc,
-            Offset.Zero);
-        var expectedEnd = new ZonedDateTime(
-            LocalDateTimePattern.GeneralIso.Parse("2022-10-08T23:59:59").Value,
-            DateTimeZone.Utc,
-            Offset.Zero);
-        
-        reportPeriod.Start.Should().Be(expectedStart);
-        reportPeriod.End.Should().Be(expectedEnd);
+        ExpectReportPeriod("2022-10-02T00:00:00", "2022-10-08T23:59:59", reportPeriod);
     }
     
     [Test]
@@ -61,17 +66,28 @@ public class ZonedDateTimeExtensionsTests
         var now = new ZonedDateTime(localDateTime, DateTimeZone.Utc, Offset.Zero);
 
         var reportPeriod = now.GetReportPeriod();
-
-        var expectedStart = new ZonedDateTime(
-            LocalDateTimePattern.GeneralIso.Parse("2022-09-25T00:00:00").Value,
-            DateTimeZone.Utc,
-            Offset.Zero);
-        var expectedEnd = new ZonedDateTime(
-            LocalDateTimePattern.GeneralIso.Parse("2022-10-01T23:59:59").Value,
-            DateTimeZone.Utc,
-            Offset.Zero);
         
-        reportPeriod.Start.Should().Be(expectedStart);
-        reportPeriod.End.Should().Be(expectedEnd);
+        ExpectReportPeriod("2022-09-25T00:00:00", "2022-10-01T23:59:59", reportPeriod);
+    }
+    
+    [Test]
+    [Description("Test case not from midnight")]
+    public void Not_calculated_from_midnight()
+    {
+        // Calendar for September / October 2022
+        // Mon Tue Wed Thu Fri Sat Sun
+        //                          25
+        //  26  27  28  29  30   1   2
+        //   3   4   5   6   7   8   9
+        //  10  11  12  13  14  15  16
+        
+        // TODO: Better variable names
+        // TODO: Better date creation?
+        var localDateTime = LocalDateTimePattern.GeneralIso.Parse("2022-10-12T11:00:00").Value;
+        var now = new ZonedDateTime(localDateTime, DateTimeZone.Utc, Offset.Zero);
+
+        var reportPeriod = now.GetReportPeriod();
+        
+        ExpectReportPeriod("2022-10-02T00:00:00", "2022-10-08T23:59:59", reportPeriod);
     }
 }
--- NodaTimePitch/ZonedDateTimeExtensions.cs
+++ NodaTimePitch/ZonedDateTimeExtensions.cs
@@ -5,6 +5,7 @@ namespace NodaTimePitch;
 
 [PublicAPI]
 // TODO: Can we make the functions pure again (e.g. by returning Maybe)?
+// TODO: Use record for reportPeriod
 public static class ZonedDateTimeExtensions
 {
     public static (ZonedDateTime Start, ZonedDateTime End) GetReportPeriod(this ZonedDateTime now)
@@ -17,13 +18,14 @@ public static class ZonedDateTimeExtensions
     private static (ZonedDateTime Start, ZonedDateTime End) GetNthReportPeriodBefore(
         this ZonedDateTime sourceDateTime, int n)
         => (
-            sourceDateTime.PreviousNth(IsoDayOfWeek.Sunday, n + 1),
-            sourceDateTime.PreviousNth(IsoDayOfWeek.Sunday, n) - Duration.FromSeconds(1));
+            sourceDateTime.PreviousNth(IsoDayOfWeek.Sunday, n + 1, LocalTime.Midnight),
+            sourceDateTime.PreviousNth(IsoDayOfWeek.Sunday, n, LocalTime.Midnight) - Duration.FromSeconds(1));
 
     // TODO: Provide variant with ZoneLocalMappingResolver -> Do we really need that?
-    private static ZonedDateTime PreviousNth(this ZonedDateTime sourceDateTime, IsoDayOfWeek dayOfWeek, int n)
+    private static ZonedDateTime PreviousNth(this ZonedDateTime sourceDateTime, IsoDayOfWeek dayOfWeek, int n,
+        LocalTime targetTimeOfDay)
     {
         var targetDate = sourceDateTime.LocalDateTime.Date.PreviousNth(dayOfWeek, n);
-        return targetDate.At(sourceDateTime.TimeOfDay).InZoneLeniently(sourceDateTime.Zone);
+        return targetDate.At(targetTimeOfDay).InZoneLeniently(sourceDateTime.Zone);
     }
 }
